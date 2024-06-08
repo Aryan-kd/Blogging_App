9d38bdb961cebadbe0d161e8c179f853b82286f4
Aryan Kodinya <aryan.kodinya@gmail.com>
2024-06-08 14:31:38 +0530

    Created Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, Dashboard, Signin, Signup, Projects, About } from "./pages";
 
 const App = () => {
-  return <div>Aryan Kodinya</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
---
+++ client/src/pages/index.js
@@ -0,0 +1,8 @@
+import About from "./About.jsx";
+import Dashboard from "./Dashboard.jsx";
+import Home from "./Home.jsx";
+import Project from "./Projects.jsx";
+import Signup from "./Signup.jsx";
+import Signin from "./Signin.jsx";
+
+export { About, Dashboard, Home, Project, Signin, Signup };
